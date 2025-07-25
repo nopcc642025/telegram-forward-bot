2bd2f0dd9b48c6b4409e68db7f8045a8b242defc
nopcc642025 <nopcustomercare@gmail.com>
2025-07-25 06:11:07 +0530

    Fix duplicate express declaration

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const express = require('express');
 
 HEAD
 // ------------web 502 error debug --------
-const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
